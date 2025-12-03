2d42112279c24aeebdcc0a566e38d95cef438857
Lucas Nogueira <97054688+LucassNogueira@users.noreply.github.com>
2025-12-03 11:16:26 -0600

    changes

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "sk_test_dummy", {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     apiVersion: "2024-10-28.acacia" as any, // Using latest or casting to any to avoid type mismatch with installed version
     typescript: true,
 });
